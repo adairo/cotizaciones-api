d5a953dd3a32908bdfa9b1f13b5d4b167d32aa03
adairo <adair.reyes450@gmail.com>
2023-05-16 12:06:20 -0600

    serve index.html as static file

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json())
+app.use("/", express.static("public"))
 app.use("/api/productos", productsRouter)
 app.use("/api/cotizaciones", cotizacionesRouter)
 
